4caab05e44e0a2869fd605054447009a33ac4200
Maria Clara <128990391+Mary-cb@users.noreply.github.com>
2024-07-08 14:46:27 -0300

    feat: Swagger implemented

--- mscustomer/src/main/java/uol/compass/desafiopb/mscustomer/MscustomerApplication.java
+++ mscustomer/src/main/java/uol/compass/desafiopb/mscustomer/MscustomerApplication.java
@@ -3,9 +3,11 @@ package uol.compass.desafiopb.mscustomer;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableRabbit
+@EnableFeignClients
 public class MscustomerApplication {
 
 	public static void main(String[] args) {
